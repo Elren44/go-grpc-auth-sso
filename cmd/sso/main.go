a668b07f3bad75ce19e5e2b8bb7f18729ffec267
Sergey Rogozin <s.rogozin@vlig44.ru>
2025-10-27 15:55:55 +0300

    init

---
+++ cmd/sso/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Elren44/go-grpc-auth-sso/internal/config"
+	"github.com/Elren44/go-grpc-auth-sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	log.Info("Config loaded", slog.Any("cfg", cfg))
+	log.Debug("Debug message")
+	log.Warn("Warn message")
+	log.Error("Error message")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
